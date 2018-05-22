c5060a251acb5173057dfe8fad7b470ca59118f1
Luis Betancourt <webdeveloperpr@gmail.com>
2018-05-21 20:59:59 -0400

    Create docs folder

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const DB_NAME = 'my-project';
 const TEST_DB_NAME = 'my-project-test';
-const DIST_DIR = path.join(__dirname, '../dist');
+const DIST_DIR = path.join(__dirname, '../docs');
 const ENTRY_FILE = path.join(__dirname, '../client/index.js');
 const TEMPLATES_DIR = path.join(__dirname, 'templates');
 const DIST_DIR_INDEX_FILE = path.join(DIST_DIR, 'index.html');
